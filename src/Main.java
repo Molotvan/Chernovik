56a89ff5c613d8d247391dd5c7bbadb4c94c2410
Иван Молотков <molotvan@yandex.ru>
2022-03-30 01:13:48 +0300

    World

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.time.LocalDateTime;
 public class Main {
     public static void main(String[] args) {
        System.out.println(LocalDateTime.now());
-       System.out.println("Hello");
+//       System.out.println("Hello");
+        System.out.println("World");
     }
 }
