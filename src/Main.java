66373e861bfa72af6f4c050eb5dfbee2b00657e1
Molotvan <molotvan@yandex.ru>
2022-03-30 01:56:16 +0300

    не беспокойся

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ import java.time.LocalDateTime;
 public class Main {
     public static void main(String[] args) {
        System.out.println(LocalDateTime.now());
-//       System.out.println("Hello");
+       System.out.println("Hello");
         System.out.println("World");
+        System.out.println("Don`t worry");
     }
 }
